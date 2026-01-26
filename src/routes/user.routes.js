81bed566879ece4c68ba00dca99f33697d576688
PIYUSH <piyushagrawal0213@gmail.com>
2026-01-27 03:09:04 +0530

    add: implement user account management routes for password change, current user retrieval, and account updates

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
-import { loginUser,logoutUser,registerUser,refreshAccessToken } from "../controllers/user.controllers.js";
+import { loginUser,logoutUser,registerUser,
+    refreshAccessToken, changeCurrentPassword, getCurrentUser,
+     updateAccountDetails, updateUserAvatar, updateUserCoverImage,
+      getUSerChannelProfile, getWatchHistory } 
+      from "../controllers/user.controllers.js";
 
 import {upload} from "../middlewares/multer.middleware.js"
 
@@ -23,6 +27,22 @@ router.route("/logout").post(verifyJWT ,logoutUser)
 
 //refresh token ka end point
 router.route("/refresh-token").post(refreshAccessToken)  
+
+router.route("/change-password").post(verifyJWT,changeCurrentPassword)
+
+router.route("/current-user").get(verifyJWT,getCurrentUser)
+
+router.route("/update_account").patch(verifyJWT,updateAccountDetails)
+
+router.route("/avatar").patch(verifyJWT,upload.single ("avatar"),updateUserAvatar)
+
+router.route("/cover-Image").patch(verifyJWT,upload.single("/coverImage"),updateUserCoverImage)
+
+//we are geeting from params username
+
+router.route("/c/:username").get(verifyJWT,getUSerChannelProfile)
+
+router.route("/history").get(verifyJWT,getWatchHistory)
 
 
 
